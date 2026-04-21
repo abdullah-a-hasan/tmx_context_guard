1174a3080568a84befb6580eb228c5a11a31809a
Abdullah A. Hasan <alsheikh.hasan@gmail.com>
2026-04-21 09:38:45 -0500

    added labels

--- tmx_ice_guard/tmx_context_defs.py
+++ tmx_ice_guard/tmx_context_defs.py
@@ -1,15 +1,17 @@
 import hashlib
 import zlib
 
-#TMX platform definitions
+# TMX platform definitions
 tmx_context_defs = {}
 
 # Phrase/Memsource
-tmx_context_defs['transifix'] = {"prev_text_prop": "context",
+tmx_context_defs['transifix'] = {"label": "Transifix",
+                                 "prev_text_prop": "context",
                                  "context_level": "tu"
                                  }
 
-tmx_context_defs['phrase'] = {"prev_text_prop": "context_prev",
+tmx_context_defs['phrase'] = {"label": "Phrase",
+                              "prev_text_prop": "context_prev",
                               "next_text_prop": "context_next",
                               "context_level": "tuv",
                               "remove_props": ["created_by", "modified_by"],
@@ -17,7 +19,8 @@ tmx_context_defs['phrase'] = {"prev_text_prop": "context_prev",
                               "project_prop": "project",
                               }
 
-tmx_context_defs['memoq'] = {"prev_text_prop": "x-context-pre",
+tmx_context_defs['memoq'] = {"label": "memoQ",
+                             "prev_text_prop": "x-context-pre",
                              "next_text_prop": "x-context-post",
                              "context_level": "tuv",
                              "remove_props": ["client", "domain", "subject", "corrected", "aligned", "x-Project", "x-Client", "x-Domain"],
@@ -26,7 +29,8 @@ tmx_context_defs['memoq'] = {"prev_text_prop": "x-context-pre",
                              }
 
 # XTM
-tmx_context_defs['xtm'] = {"prev_text_prop": "x-previous-source-text",
+tmx_context_defs['xtm'] = {"label": "XTM",
+                           "prev_text_prop": "x-previous-source-text",
                            "next_text_prop": "x-next-source-text",
                            "prev_hash_prop": "x-previous-crc",
                            "next_hash_prop": "x-next-crc",
@@ -36,7 +40,8 @@ tmx_context_defs['xtm'] = {"prev_text_prop": "x-previous-source-text",
                            }
 
 # GlobalLink/WordFast
-tmx_context_defs['gl'] = {"prev_text_prop": "previousMd5Segment",
+tmx_context_defs['gl'] = {"label": "TransPerfect (GL/Wordfast)",
+                          "prev_text_prop": "previousMd5Segment",
                           "next_text_prop": "nextMd5Segment",
                           "prev_hash_prop": "previousMd5Checksum",
                           "next_hash_prop": "nextMd5Checksum",
@@ -45,9 +50,9 @@ tmx_context_defs['gl'] = {"prev_text_prop": "previousMd5Segment",
                           }
 
 # Trados
-tmx_context_defs['trados'] = {"prev_text_prop": "x-ContextContent",
+tmx_context_defs['trados'] = {"label": "RWS (Trados)",
+                              "prev_text_prop": "x-ContextContent",
                               "remove_props": ["x-Context", "x-Origin", "x-ConfirmationLevel", "x-StructureContext:MultipleString", "x-LastUsedBy", "x-ContextContent",
                                                "x-OriginalFormat"],
                               "context_level": "tu"
-                              #"replacements": [(r' \| .+$', '')],
                               }
